f277dd50041eb719c6a48a570b17175ae1a065f5
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-04-27 14:52:16 +0600

    Messenger

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,6 +3,7 @@ import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
 import { ToastContainer } from "react-toastify";
 import { useEffect } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 import Aos from "aos";
 import "aos/dist/aos.css";
 // import { AuthContext } from "../../provider/AuthProvider";
@@ -11,7 +12,7 @@ import "aos/dist/aos.css";
 const MainPage = () => {
   //   const {loading} = useContext(AuthContext)
   //   if (loading) {
-  //     return <div className=" h-screen flex justify-center items-center">
+  //     return <div className="flex items-center justify-center h-screen ">
   //         <span className="loading loading-spinner loading-lg"></span>
   //     </div>
   // }
@@ -21,13 +22,20 @@ const MainPage = () => {
   }, []);
   return (
     <div className="">
-       <div
-      data-aos="fade-down"
-      data-aos-offset="100"
-      data-aos-easing="ease-in-sine"
-      data-aos-duration="1000"
-      className=" bg-gray-100"
-    ></div>
+      {/* ---------------messenger------------------- */}
+      <MessengerCustomerChat
+        pageId="<272520735953054>"
+        appId="<1580465442749663>"
+        htmlRef="<REF_STRING>"
+      />
+      {/* ----------------------------- */}
+      <div
+        data-aos="fade-down"
+        data-aos-offset="100"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+        className="bg-gray-100 "
+      ></div>
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -43,8 +51,8 @@ const MainPage = () => {
       {/* Same as */}
       <ToastContainer />
       <Nav></Nav>
-      <div className=" min-h-screen">
-      <Outlet></Outlet>
+      <div className="min-h-screen ">
+        <Outlet></Outlet>
       </div>
       <Footer></Footer>
     </div>
